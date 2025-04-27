71ac91f9f4af4093b872082b9392d260d67c8ef6
khuattiendat <khuattiendat1@gmail.com>
2025-04-27 14:28:39 +0700

    up

--- backend/src/config/connectDB.js
+++ backend/src/config/connectDB.js
@@ -1,17 +1,17 @@
 const {Sequelize} = require('sequelize')
 require('dotenv').config();
 // production
-// const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-//     host: process.env.DATABASE_HOST,
-//     dialect: 'mysql',
-//     timezone: "+07:00",
-// });
-// dev
-const sequelize = new Sequelize('my_project', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+    host: process.env.DATABASE_HOST,
     dialect: 'mysql',
     timezone: "+07:00",
 });
+// dev
+// const sequelize = new Sequelize('my_project', 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     timezone: "+07:00",
+// });
 const checkConnect = async () => {
     try {
         await sequelize.authenticate();
